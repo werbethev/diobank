bc2e745700b99c802ecaf9ceca05cc30bba5473f
werbethe <werbethe@medware.com.br>
2021-03-28 11:05:27 -0300

    adicionado projeto de teste e teste do metodo sacar

--- diobank/Classes/Conta.cs
+++ diobank/Classes/Conta.cs
@@ -1,12 +1,13 @@
 ﻿using diobank.Enum;
+using System;
 
 namespace diobank.Classes
 {
     public class Conta
     {
+        public double Credito { get; private set; }
         public string Nome { get; private set; }
         public double Saldo { get; private set; }
-        public double Credito { get; private set; }
         public eTipoConta Tipo { get; private set; }
         public Conta(string nome, double saldo, double credito, eTipoConta tipo)
         {
@@ -14,6 +15,26 @@ namespace diobank.Classes
             Saldo = saldo;
             Credito = credito;
             Tipo = tipo;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (valorSaque <= 0)
+            {
+                Console.WriteLine($"O valor {valorSaque.ToString("0.00")} é inválido!");
+                return false;
+            }
+
+            if (Saldo - valorSaque < -Credito)
+            {
+                Console.WriteLine("Saldo insuficiente!");
+                return false;
+            }
+
+            Saldo -= valorSaque;
+            Console.WriteLine($"Saldo atual da conta de {Nome} é {Saldo}");
+
+            return true;
         }
     }
 }
---
+++ diobankTeste/Classes/ContaTeste.cs
@@ -0,0 +1,76 @@
+﻿using diobank.Classes;
+using diobank.Enum;
+using Xunit;
+using Xunit.Sdk;
+
+namespace diobankTeste.Classes
+{
+    public class ContaTeste
+    {
+        [Fact]
+        public void Construtor_PropriedadesDevemSerIguaisAoQueFoiPassado()
+        {
+            const string nomeConta = "Conta 1";
+            const double saldo = 10;
+            const double credito = 20;
+            const eTipoConta pessoaFisica = eTipoConta.PessoaFisica;
+            var conta = new Conta(nomeConta, saldo, credito, pessoaFisica);
+
+            Assert.Equal(nomeConta, conta.Nome);
+            Assert.Equal(saldo, conta.Saldo);
+            Assert.Equal(credito, conta.Credito);
+            Assert.Equal(pessoaFisica, conta.Tipo);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 100, false)]
+        [InlineData(50, 0, 100, true)]
+        [InlineData(150, 0, 100, false)]
+        [InlineData(100, 0, 100, true)]
+        [InlineData(150, 50, 100, true)]
+        [InlineData(50, 50, 0, true)]
+        [InlineData(100, 50, 0, false)]
+        public void Sacar(double valorSaque, double saldo, double credito, bool resultadoEsperado)
+        {
+            var conta = new Conta("", saldo, credito, eTipoConta.PessoaFisica);
+
+            bool retorno = conta.Sacar(valorSaque);
+
+            Assert.Equal(resultadoEsperado, retorno);
+        }
+
+        [Theory]
+        [InlineData(0, 100)]
+        [InlineData(10, 90)]
+        [InlineData(100, 0)]
+        [InlineData(150, -50)]
+        [InlineData(200, -100)]
+        [InlineData(250, 100)]//saque inválido continua mesmo saldo
+        public void Sacar_DeveAlterarValorSaldo(double valorSaque, double saldoEsperado)
+        {
+            var conta = new Conta("", 100, 100, eTipoConta.PessoaFisica);
+
+            conta.Sacar(valorSaque);
+
+            Assert.Equal(saldoEsperado, conta.Saldo);
+        }
+
+        [Fact]
+        public void Sacar_VariosSquesMesmaCnotaDeveAlterarSaldo()
+        {
+            var conta = new Conta("", 100, 100, eTipoConta.PessoaFisica);
+
+            conta.Sacar(10);
+            Assert.Equal(90, conta.Saldo);
+
+            conta.Sacar(80);
+            Assert.Equal(10, conta.Saldo);
+
+            conta.Sacar(10);
+            Assert.Equal(0, conta.Saldo);
+
+            conta.Sacar(50);
+            Assert.Equal(-50, conta.Saldo);
+        }
+    }
+}
